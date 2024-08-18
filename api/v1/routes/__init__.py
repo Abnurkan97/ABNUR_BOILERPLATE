8fa0b25c1696bac90637cf5f5b6fd3320ae59068
ABDULAZIZ NURA KANI <abdulnurakani@gmail.com>
2024-08-19 00:05:32 +0530

    update

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,8 +1,5 @@
 from fastapi import APIRouter
-from api.v1.routes.auth import auth
-from api.v1.routes.user import user
-
+from api.v1.routes.register_user import register
 
 api_version_one = APIRouter(prefix="/api/v1")
-api_version_one.include_router(auth)
-api_version_one.include_router(user)
+api_version_one.include_router(register)
---
+++ api/v1/routes/register_user.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, status, APIRouter, Response, Request,HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session, relationship
+from api.utils.success_response import success_response
+from api.v1.models import User
+from typing import Annotated
+from datetime import datetime, timedelta
+
+from api.v1.schemas.user import UserCreate
+from api.v1.schemas.token import EmailRequest, TokenRequest
+from api.utils.email_service import send_mail
+from api.db.database import get_db
+from api.v1.services.user import user_service
+
+register = APIRouter(prefix="/reg", tags=["registration"])
+
+  
+@register.post("/register", status_code=status.HTTP_201_CREATED)
+def register(user_schema: UserCreate, db: Session = Depends(get_db)):
+    '''Endpoint for a user to register their account'''
+
+    #check if email exist
+    check_email = db.query(User).filter(User.email == user_schema.email).first()
+    
+    if check_email:
+        raise HTTPException(status_code=400,detail="Email already exist")
+    
+    hashed_password = user_service.hash_password(user_schema.password)
+    user_schema.password = hashed_password
+
+    new_user = User(**user_schema.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"message":"registered succesfully"}
